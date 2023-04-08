41e99cbbffe5898405738e063908d4809a506749
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-04-08 17:40:39 -0300

    Relações SQL 2

--- typescript-apis-express/src/database/labecommerce.sql
+++ typescript-apis-express/src/database/labecommerce.sql
@@ -16,10 +16,18 @@ CREATE TABLE products (
 CREATE TABLE purchases (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     total_price REAL NOT NULL,
-    paid INTEGER NOT NULL,
-    delivered_at TEXT,
+    paid INTEGER DEFAULT(0) NOT NULL,
+    delivered_at TEXT DEFAULT (DATETIME()) NOT NULL,
     buyer_id TEXT NOT NULL,
     FOREIGN KEY (buyer_id) REFERENCES users(id) 
+);
+
+CREATE TABLE purchases_products (
+    purchase_id TEXT NOT NULL,
+    product_id TEXT NOT NULL,
+    quantity INTEGER DEFAULT(1) NOT NULL,
+    FOREIGN KEY (purchase_id) REFERENCES purchases(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 INSERT INTO users (id, email, password)
@@ -36,12 +44,12 @@ VALUES
 ("P04", "Tomada Inteligente", 89.90, "Acessórios"),
 ("P05", "Fone de Ouvido - Sem fio", 458.00, "Acessórios");
 
-INSERT INTO purchases 
+INSERT INTO purchases (id, total_price, paid, buyer_id)
 VALUES
-("C01", 989.90, 1, NULL, "U01"),
-("C02", 2576.00, 0, NULL, "U01"),
-("C03", 458.00, 0, NULL, "U02"),
-("C04", 2419.00, 1, "25/03/2023", "U02");
+("C01", 789.00, 1, "U01"),
+("C02", 2149.00, 1, "U01"),
+("C03", 916.00, 1, "U02"),
+("C04", 359.60, 1, "U02");
 
 INSERT INTO users
 VALUES
@@ -50,6 +58,13 @@ VALUES
 INSERT INTO products
 VALUES
 ("P06", "Ar Condicionado", 1.712, "Eletrodoméstico");
+
+INSERT INTO purchases_products
+VALUES
+("C01", "P02", 1),
+("C02", "P01", 1),
+("C03", "P05", 2),
+("C04", "P04", 3);
 
 SELECT * FROM products AS getAllProducts;
 
@@ -76,10 +91,26 @@ WHERE price >= 500.00
 AND price <= 1200.00
 ORDER BY price ASC;
 
-SELECT purchases.id, users.id, users.email, purchases.total_price, purchases.paid, purchases.delivered_at 
+SELECT purchases.id AS idPurchases, users.id AS idUsers, 
+users.email, purchases.total_price, 
+purchases.paid, purchases.delivered_at 
 FROM purchases
 INNER JOIN users
 ON purchases.buyer_id = users.id; 
+
+SELECT purchase_id AS idPurchases, purchases.buyer_id AS idUsers,
+product_id AS idProducts, products.name  AS productsName,
+products.price, quantity, purchases.total_price 
+FROM purchases_products
+INNER JOIN products ON product_id = products.id
+INNER JOIN purchases ON purchase_id = purchases.id;
+
+UPDATE purchases 
+SET total_price = 
+((select price from products WHERE id = "P04")
+*
+(select quantity from purchases_products WHERE product_id = "P04"))
+WHERE id = "C04";
 
 UPDATE products
 SET PRICE = 789
@@ -90,7 +121,7 @@ SET email = "ludmilasouza31@gmail.com"
 WHERE id = "U01";
 
 UPDATE purchases
-SET delivered_at = "04/04/2023"
+SET delivered_at = DATETIME('now', 'localtime')
 WHERE id = "C01";
 
 DELETE FROM users
